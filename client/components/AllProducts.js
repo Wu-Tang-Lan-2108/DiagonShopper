cf4dfbf7c1a6e2d942a061030903ab99c9178126
Grace Tveit <grace.tveit@gmail.com>
2021-09-21 13:32:43 -0700

    feat: All Product View
    Added component & redux store to view all products

---
+++ client/components/AllProducts.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { connect } from "react-redux";
+import { fetchProducts } from "../store/allProducts";
+import { Link } from "react-router-dom";
+class AllProducts extends React.Component {
+  componentDidMount() {
+    this.props.fetchProducts();
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.products ? (
+          <ul>
+            {this.props.products.map((product) => {
+              return (
+                <li key={product.id}>
+                  <Link to={`/products/${product.id}`}>
+                    <h2>{product.name}</h2>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <React.Fragment />
+        )}
+      </div>
+    );
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    fetchProducts: () => dispatch(fetchProducts()),
+  };
+};
+
+const mapState = (state) => {
+  return {
+    products: state.allProducts,
+  };
+};
+
+export default connect(mapState, mapDispatch)(AllProducts);
