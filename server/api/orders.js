432eea39d6f7cd687832306b39be1a64f583fad9
Grace Tveit <grace.tveit@gmail.com>
2021-09-28 08:34:52 -0700

    feat: CART; Adding more routes to /api/orders

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {
-  models: { Order },
+  models: { Order, CartItem, Product },
 } = require('../db');
 module.exports = router;
 
@@ -13,12 +13,84 @@ router.get('/:orderId', async (req, res, next) => {
   }
 });
 
+router.get('/:userId/all', async (req, res, next) => {
+  try {
+    const orders = await Order.findAll({
+      where: {
+        userId: req.params.userId,
+      },
+      include: {
+        model: CartItem,
+        include: {
+          model: Product,
+        },
+      },
+    });
+    res.send(orders);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GETs current order for specified user
+router.get('/:userId/current', async (req, res, next) => {
+  try {
+    const order = await Order.findOne({
+      where: {
+        userId: req.params.userId,
+        status: 'CURRENT',
+      },
+      include: {
+        model: CartItem,
+        include: {
+          model: Product,
+        },
+      },
+    });
+    res.send(order);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:userId/past', async (req, res, next) => {
+  try {
+    const orders = await Order.findAll({
+      where: {
+        userId: req.params.userId,
+        status: 'PAST',
+      },
+      include: {
+        model: CartItem,
+        include: {
+          model: Product,
+        },
+      },
+    });
+    res.send(orders);
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.post('/', async (req, res, next) => {
   try {
     const order = await Order.create(req.body);
     if (order) {
       res.send(order);
     } else next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put('/:orderId', async (req, res, next) => {
+  try {
+    await Order.update(req.body, { where: { id: req.params.orderId } });
+    const order = await Order.findByPk(req.params.orderId, {
+      include: { model: CartItem, incldue: { model: Product } },
+    });
+    res.send(order);
   } catch (error) {
     next(error);
   }
